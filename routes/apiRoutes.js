c181688525bff5751e3c67d28f8206ea0724c5bf
Ryyder <brucecho82@hotmail.com>
2019-06-08 12:43:44 -0700

    changed variable name
    changed table name in default example.js file (i.e db.Example.create(req.body)...) to db.Students.create(req.body)...)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Students.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+    db.Students.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+    db.Students.destroy({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.json(dbExample);
@@ -26,7 +26,7 @@ module.exports = function(app) {
 
   // Update
   app.put("/api/examples", function(req, res) {
-    db.Example.update(req.body, 
+    db.Students.update(req.body, 
       {
         where: {
           id: req.body.id
