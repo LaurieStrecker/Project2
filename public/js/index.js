b464bc365d0fdc9724bda15e329059832e38856d
Kai R <kairichardson2000@gmail.com>
2019-06-08 21:27:24 -0700

    adding google auth

--- public/js/index.js
+++ public/js/index.js
@@ -97,3 +97,18 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+}
--- server.js
+++ server.js
@@ -1,16 +1,75 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var RedisStore = require("connect-redis")(session);
+var GoogleStrategy = require("../").Strategy;
 
 var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+// Google auth
+var GOOGLE_CLIENT_ID =
+  "30634033579-46upcq5quevugp6473gqqo79hbn2vomk.apps.googleusercontent.com";
+var GOOGLE_CLIENT_SECRET = "e6chWqv2hQ_dB6Ju6hX6yEx-";
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use the GoogleStrategy within Passport.
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+      passReqToCallback: true
+    },
+    function(request, accessToken, refreshToken, profile, done) {
+      // asynchronous verification, for effect...
+      process.nextTick(function() {
+        return done(null, profile);
+      });
+    }
+  )
+);
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(
+  session({
+    secret: "cookie_secret",
+    name: "kaas",
+    store: new RedisStore({
+      host: "127.0.0.1",
+      port: 6379
+    }),
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
@@ -37,7 +96,7 @@ if (process.env.NODE_ENV === "test") {
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
       PORT
     );
