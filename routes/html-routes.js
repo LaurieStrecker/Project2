aaa4485d24ec8966a3a4721975e86c83b2bc2375
Kai R <kairichardson2000@gmail.com>
2019-06-09 21:53:05 -0700

    adding cip code

---
+++ routes/html-routes.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  // new user route loads newUser.html
+  app.get("/newuser", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/newUser.html"));
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
