c46378455b32defce35f91f5a74e4f4318fdf5c2
Mark Silva <mark.allen.silva@gmail.com>
2019-06-10 21:11:09 -0700

    dashboard updates

---
+++ public/js/dashboard.js
@@ -0,0 +1,6 @@
+
+$("#firstNameDisplay").append("Cleatus");
+$("#schoolNameDisplay").append("Pudget Sound University");
+$("#edLvlDisplay").append("Associate's");
+$("#majorDisplay").append("Business");
+$("#prgEndDisplay").append("June 19, 2022");
