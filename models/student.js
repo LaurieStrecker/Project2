ea73a86682a68fb3f90392d2523a38a47be6328b
Kai Richardson <kairichardson2000Gmail.com>
2019-06-11 20:06:15 -0700

    fixing heroku

--- models/student.js
+++ models/student.js
@@ -41,13 +41,6 @@ module.exports = function(sequelize, DataTypes) {
         len: [1]
       }
     },
-    google_id: {
-      type: DataTypes.INTEGER(100),
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
     school_name: {
       type: DataTypes.STRING(50),
       allowNull: false,
@@ -96,6 +89,14 @@ module.exports = function(sequelize, DataTypes) {
       defaultValue: "active"
     }
   });
+
+  Students.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Students.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
 
   return Students;
 };
