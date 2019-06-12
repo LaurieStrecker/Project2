dd158a09a677c876e46325f101a039d9a02977f3
Ryyder <brucecho82@hotmail.com>
2019-06-11 21:06:20 -0700

    updated models
    updated Students model, post request and get request

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,3 +4,13 @@
 // // $("#edLvlDisplay").append("Associate's");
 // // $("#majorDisplay").append("Business");
 // // $("#prgEndDisplay").append("June 19, 2022");
+
+$.ajax({
+  headers: {
+    "Content-Type": "application/json"
+  },
+  url: "api/students/:id",
+  type: "GET"
+}).then(function(res) {
+  console.log(res);
+});
