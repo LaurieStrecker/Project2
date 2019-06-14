cdf8b08ddb275f39502bc141a72389676a170911
Ryyder <brucecho82@hotmail.com>
2019-06-13 18:46:07 -0700

    update method
    update button is working on updateUser.html

--- public/js/updateUser.js
+++ public/js/updateUser.js
@@ -1,4 +1,4 @@
-$("#lastName").val("Lannister");
+/* $("#lastName").val("Lannister");
 $("#firstName").val("Jamie");
 $("#email").val("jamieonehand@gmail.com");
 $("#phone").val("555-666-7777");
@@ -7,4 +7,95 @@ $("#startDate").val("2019-09-24");
 $("#endDate").val("2021-06-15");
 $("#edLvl").val("bachelors");
 $("#termType").val("quarter");
-$("#cipCode").val("11.0101");
+$("#cipCode").val("11.0101"); */
+
+function getUser() {
+  $.ajax({
+    url: "api/students/5",
+    type: "GET"
+  }).then(function(res) {
+    console.log(res);
+    $("#lastName").val(res.lastname);
+    $("#firstName").val(res.firstname);
+    $("#email").val(res.email);
+    $("#phone").val(res.phone);
+    $("#schoolName").val(res.school_name);
+    $("#startDate").val(res.program_start);
+    $("#endDate").val(res.program_end);
+    $("#edLvl").val(res.ed_level);
+    $("#termType").val(res.qt_sem);
+    $("#cipCode").val(res.cip_code_one);
+  });
+}
+
+getUser();
+
+//update user info event
+$("#updateInfo").on("click", function(event) {
+  event.preventDefault();
+  console.log(event);
+  var lastName = $("#lastName")
+    .val()
+    .trim();
+  var firstName = $("#firstName")
+    .val()
+    .trim();
+  var phone = $("#phone")
+    .val()
+    .trim();
+  var termType = $("#termType")
+    .val()
+    .trim();
+  var endDate = $("#endDate")
+    .val()
+    .trim();
+  var cipCode = $("#cipCode")
+    .val()
+    .trim();
+  var schoolName = $("#schoolName")
+    .val()
+    .trim();
+  var startDate = $("#startDate")
+    .val()
+    .trim();
+  var edLvl = $("#edLvl")
+    .val()
+    .trim();
+  var email = $("#email")
+    .val()
+    .trim();
+
+  var updateUser = {
+    firstname: firstName,
+    lastname: lastName,
+    phone: phone,
+    school_name: schoolName,
+    qt_sem: termType,
+    program_start: startDate,
+    program_end: endDate,
+    ed_level: edLvl,
+    cip_code_one: cipCode,
+    email: email
+  };
+
+  console.log(updateUser);
+
+  $.ajax({
+    headers: {
+      "Content-Type": "application/json"
+    },
+    url: "/api/students/1",
+    type: "PUT",
+    data: JSON.stringify(updateUser)
+  }).then(getUser);
+});
+
+//update ajax call
+/* function updateUser(id) {
+  $.ajax({
+    url: "api/students/" + id,
+    type: "PUT"
+  }).then(getUser);
+}
+
+updateUser(thing); */
