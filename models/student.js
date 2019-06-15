1eee2bea3892d5b44e9414e8fbf91efb324d317f
Ryyder <brucecho82@hotmail.com>
2019-06-15 15:34:31 -0700

    Update student.js
    allownull take out

--- models/student.js
+++ models/student.js
@@ -7,28 +7,24 @@ module.exports = function(sequelize, DataTypes) {
     }, */
     firstname: {
       type: DataTypes.STRING(50),
-      allowNull: false,
       validate: {
         len: [1]
       }
     },
     lastname: {
       type: DataTypes.STRING(50),
-      allowNull: false,
       validate: {
         len: [1]
       }
     },
+    googleId: {
+      type: DataTypes.STRING(100)
+    },
     username: {
       type: DataTypes.TEXT
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
     email: {
       type: DataTypes.STRING(50),
-      allowNull: false,
       validate: {
         isEmail: true,
         len: [1]
@@ -36,28 +32,24 @@ module.exports = function(sequelize, DataTypes) {
     },
     phone: {
       type: DataTypes.STRING(50),
-      allowNull: false,
       validate: {
         len: [1]
       }
     },
     school_name: {
       type: DataTypes.STRING(50),
-      allowNull: false,
       validate: {
         len: [1]
       }
     },
     qt_sem: {
       type: DataTypes.STRING(50),
-      allowNull: false,
       validate: {
         len: [1]
       }
     },
     program_start: {
       type: DataTypes.DATEONLY,
-      allowNull: false,
       validate: {
         isDate: true,
         len: [1]
@@ -65,7 +57,6 @@ module.exports = function(sequelize, DataTypes) {
     },
     program_end: {
       type: DataTypes.DATEONLY,
-      allowNull: false,
       validate: {
         isDate: true,
         len: [1]
@@ -76,7 +67,6 @@ module.exports = function(sequelize, DataTypes) {
     },
     cip_code_one: {
       type: DataTypes.DECIMAL(6, 4),
-      allowNull: false,
       validate: {
         len: [1]
       }
