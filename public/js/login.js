608b75ae00b07373d6dc5647a8b38abe57bcb49c
Kai Richardson <kairichardson2000Gmail.com>
2019-06-15 11:02:50 -0700

    merging

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,4 @@
-
-
-$submitBtn.on("click", handleFormSubmit);
+function GetSelectedItem() {
+  // eslint-disable-next-line quotes
+  $('a[href="<GENERATED_GOOGLE_URL>"]').attr("href", "http://www.live.com/");
+}
