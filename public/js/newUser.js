88c85ecccac325efb629da165a92c3d4ac5f6886
Mark Silva <mark.allen.silva@gmail.com>
2019-06-14 18:28:51 -0700

    basic form validation complete

--- public/js/newUser.js
+++ public/js/newUser.js
@@ -94,6 +94,7 @@ var handleDeleteBtnClick = function() {
   });
 };
 
+
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick); */
@@ -101,62 +102,78 @@ $exampleList.on("click", ".delete", handleDeleteBtnClick); */
 $("#submit").on("click", function(event) {
   event.preventDefault();
   console.log(event);
-  var lastName = $("#lastName")
-    .val()
-    .trim();
-  var firstName = $("#firstName")
-    .val()
-    .trim();
-  var phone = $("#phone")
-    .val()
-    .trim();
-  var termType = $("#termType")
-    .val()
-    .trim();
-  var endDate = $("#endDate")
-    .val()
-    .trim();
-  var cipCode = $("#cipCode")
-    .val()
-    .trim();
-  var schoolName = $("#schoolName")
-    .val()
-    .trim();
-  var startDate = $("#startDate")
-    .val()
-    .trim();
-  var edLvl = $("#edLvl")
-    .val()
-    .trim();
-  var email = $("#email")
-    .val()
-    .trim();
 
-  var newStudent = {
-    firstname: firstName,
-    lastname: lastName,
-    phone: phone,
-    school_name: schoolName,
-    qt_sem: termType,
-    program_start: startDate,
-    program_end: endDate,
-    ed_level: edLvl,
-    cip_code_one: cipCode,
-    email: email
-  };
+  // Form validation
+  function validateForm() {
+    var isValid = true;
+    $("input").each(function() {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+    return isValid;
+  }
 
-  console.log(newStudent);
+  if (validateForm()) {
+    var lastName = $("#lastName")
+      .val()
+      .trim();
+    var firstName = $("#firstName")
+      .val()
+      .trim();
+    var phone = $("#phone")
+      .val()
+      .trim();
+    var termType = $("#termType")
+      .val()
+      .trim();
+    var endDate = $("#endDate")
+      .val()
+      .trim();
+    var cipCode = $("#cipCode")
+      .val()
+      .trim();
+    var schoolName = $("#schoolName")
+      .val()
+      .trim();
+    var startDate = $("#startDate")
+      .val()
+      .trim();
+    var edLvl = $("#edLvl")
+      .val()
+      .trim();
+    var email = $("#email")
+      .val()
+      .trim();
 
-  // use a post call to save all student info to our mysql db
-  $.ajax({
-    headers: {
-      "Content-Type": "application/json"
-    },
-    url: "/api/students",
-    type: "POST",
-    data: JSON.stringify(newStudent)
-  }).then(function(res) {
-    console.log(res);
-    console.log("info saved to database");
-  });
+    var newStudent = {
+      firstname: firstName,
+      lastname: lastName,
+      phone: phone,
+      school_name: schoolName,
+      qt_sem: termType,
+      program_start: startDate,
+      program_end: endDate,
+      ed_level: edLvl,
+      cip_code_one: cipCode,
+      email: email
+    };
+
+    console.log(newStudent);
+
+    // use a post call to save all student info to our mysql db
+    // $.ajax({
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   url: "/api/students",
+    //   type: "POST",
+    //   data: JSON.stringify(newStudent)
+    // }).then(function(res) {
+    //   console.log(res);
+    //   console.log("info saved to database");
+    // });
+  } else {
+    alert("Please complete the entire form!");
+  }
 });
